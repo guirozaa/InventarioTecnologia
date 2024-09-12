c2770824ec0b34961edff39821de360d049262ef
Guilherme Rosa <guilhermerosa1.br@gmail.com>
2024-09-12 10:15:01 -0300

    kk

--- src/main/java/com/med_api/InventarioMedfix/mapper/EquipamentoMapper.java
+++ src/main/java/com/med_api/InventarioMedfix/mapper/EquipamentoMapper.java
@@ -5,11 +5,20 @@ import com.med_api.InventarioMedfix.dto.FornecedorDTO;
 import com.med_api.InventarioMedfix.models.Equipamento;
 import com.med_api.InventarioMedfix.models.Fornecedor;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface EquipamentoMapper {
     EquipamentoMapper INSTANCE = Mappers.getMapper(EquipamentoMapper.class);
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "nome", source = "nome")
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "nome", source = "nome")
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "nome", source = "nome")
     EquipamentoDTO equipamentoToDTO(Equipamento equipamento);
+
+
     Equipamento DTOtoEquipamento(EquipamentoDTO equipamentoDTO);
 }
